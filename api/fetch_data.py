c11b7e40a68f022f32d65d80fb78c03c9139bb63
Mohamed Gueye <mohamed.gueye88@gmail.com>
2024-11-21 11:00:30 -0500

    testing fetch for countries and seasons

--- api/fetch_data.py
+++ api/fetch_data.py
@@ -1,5 +1,8 @@
 import requests
 
+
+# Fetch league data
+
 url = "https://v3.football.api-sports.io/leagues"
 
 headers = {
@@ -9,6 +12,34 @@ headers = {
 
 response = requests.get(url, headers=headers)
 
-# Save the response content to a file because to large for terminal (testing)
 with open("leagues.json", "w") as file:
+    file.write(response.text)
+
+
+# Fetch season data
+
+url = "https://v3.football.api-sports.io/leagues/seasons"
+
+headers = {
+	"x-rapidapi-host": "v3.football.api-sports.io",
+	"x-rapidapi-key": "62774e0697625ad93954c668d07ac020"
+}
+
+response = requests.get(url, headers=headers)
+
+with open("seasons.json", "w") as file:
+    file.write(response.text)
+
+# Fetch country data
+
+url = "https://v3.football.api-sports.io/countries"
+
+headers = {
+	"x-rapidapi-host": "v3.football.api-sports.io",
+	"x-rapidapi-key": "62774e0697625ad93954c668d07ac020"
+}
+
+response = requests.get(url, headers=headers)
+
+with open("countries.json", "w") as file:
     file.write(response.text)
