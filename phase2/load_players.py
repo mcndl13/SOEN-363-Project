4b586e4a4afe2387ffbf294f606e0eeb8919ab8d
MomoG221 <146865335+MomoG221@users.noreply.github.com>
2024-12-01 23:32:15 -0500

    Update load_players.py

--- phase2/load_players.py
+++ phase2/load_players.py
@@ -1,30 +1,32 @@
 from py2neo import Node, Relationship
 from env_setup import graph
 from utils import load_csv
-from tqdm import tqdm  # Importing tqdm for the progress bar
 
 def load_players(filepath):
     data = load_csv(filepath)
     if not data:
         return
 
-    for row in tqdm(data, desc="Loading Players", unit="player"):
+    for row in data:
         try:
+            # Find the associated team
             team = graph.nodes.match("Team", team_id=int(row["teamid"])).first()
 
             if team:
+                # Create Player node
                 player = Node(
                     "Player",
                     player_id=int(row["playerid"]),
                     jersey_number=int(row["jerseynumber"]) if row["jerseynumber"] != "NULL" else None,
                     position=row["position"],
                     first_name=row["firstname"],
-                    last_name=row["lasttname"],
+                    last_name=row["LastName"],  # Matches typo in table definition
                     age=int(row["age"]) if row["age"] != "NULL" else None,
                     nationality=row["nationality"]
                 )
                 graph.merge(player, "Player", "player_id")
 
+                # Create relationship between Player and Team
                 plays_for = Relationship(player, "PLAYS_FOR", team)
                 graph.create(plays_for)
         except Exception as e:
